35f37bfaae448a8cfc07d6f49a6894c631f3938b
meghackc <meghacmohanan@gmail.com>
2022-08-03 21:24:09 +0530

    Initial Commit until combo controls
    Initial Commit until combo controls

---
+++ SeleniumCSharpNetCore/CustomControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace SeleniumCSharpNetCore
+{
+    public class CustomControl : DriverHelper
+    {
+        public static void ComboBox(String ctrlName, String value)
+        {
+            IWebElement combocontrol = driver.FindElement(By.XPath($"//input[@id='{ctrlName}-awed']"));
+            combocontrol.Clear();
+            combocontrol.SendKeys(value);
+            driver.FindElement(By.XPath($"//div[@id='{ctrlName}-dropmenu']//li[text()='{value}']"));
+
+        }
+    }
+}
---
+++ SeleniumCSharpNetCore/UnitTest1.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumCSharpNetCore
+{
+    public class Tests :DriverHelper
+    {
+        
+        [SetUp]
+        public void Setup()
+
+        {
+            Console.WriteLine("Setu");
+            driver = new ChromeDriver(@"F:\\Selenium\\SeleniumCSharpNetCore");
+        }
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+            driver.FindElement(By.XPath("//div[@id='cookieMsg']/button")).Click();
+            driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+            
+
+            driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+            
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almonds");
+
+           
+            Console.WriteLine("Test5");
+            Assert.Pass();
+            driver.Quit();
+        }
+    }
+}
